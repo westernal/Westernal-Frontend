73524c272aa63067f8556bab762889583ceae0a1
ali navidi <ali_navidi24@yahoo.com>
2022-07-21 16:10:54 +0430

    user setting's e2e test added!

--- cypress/e2e/usersetting.cy.js
+++ cypress/e2e/usersetting.cy.js
@@ -9,5 +9,18 @@ describe("Edit user", () => {
 
     //go to profile
     cy.findByRole("img", { name: /profile/i }).click({ force: true });
+
+    //click setting button
+    cy.findByRole("img", { name: /setting/i }).click();
+
+    //change the profile
+    cy.get("#image").selectFile(
+      "C:/Users/ASUS/Desktop/Folders/Pictures/WhatsApp Image 2022-05-24 at 9.30.20 PM.jpeg"
+    );
+    cy.get("#username").type("westernal");
+    cy.get("#bio").type("founder of westernal");
+    cy.get("#password").type("13791379al");
+    cy.get("#rpassword").type("13791379al");
+    cy.findByRole("button", { name: /edit/i }).click();
   });
 });
