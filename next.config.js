d55f6e6fefbc69181967d1196a5081a30ef7d535
ali navidi <ali_navidi24@yahoo.com>
2022-07-28 20:01:48 +0430

    host changed

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
-const withPWA = require('next-pwa')
+const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public'
-  }
-})
+    dest: "public",
+  },
+});
 module.exports = {
   images: {
-    domains: ['localhost'],
+    domains: ["localhost", "alinavidi.ir"],
   },
-}
+};
