b739bb151cc74b98f3a376173e16bed8fd2d74c5
ali navidi <ali_navidi24@yahoo.com>
2022-07-31 13:22:48 +0430

    access controled

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import "../styles/style.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      router.push("/");
+    }
+  }, []);
   return (
     <>
       <ToastContainer
