b31ebf684cb6caec29ce2ac8f9e4cdbd4d7eb7a2
ali navidi <ali_navidi24@yahoo.com>
2022-07-31 16:07:02 +0430

    PWA modal added!

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Index() {
       await sleep(1500);
       if (localStorage.getItem("token")) {
         router.push("/home");
-      }
-      if (document.getElementsByClassName("loader")[0]) {
+      } else if (document.getElementsByClassName("loader")[0]) {
         document.getElementsByClassName("loader")[0].style.display = "none";
         document.getElementsByClassName("login")[0].style.display = "flex";
         document.getElementsByClassName("modal")[0].style.display = "block";
