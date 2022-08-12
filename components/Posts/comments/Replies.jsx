0aa8f3c51ff1c25933015edaa0cb7f1d687e8747
ali navidi <ali_navidi24@yahoo.com>
2022-08-13 03:01:23 +0430

    reply added

--- components/Posts/comments/Replies.jsx
+++ components/Posts/comments/Replies.jsx
@@ -1,51 +1,10 @@
-import { useState } from "react";
-import jwtDecode from "jwt-decode";
-import Link from "next/link";
-import dateFormat from "dateformat";
-import DeleteComment from "./DeleteComment";
+import Reply from "./Reply";
 
 const Replies = ({ replies, onDelete }) => {
-  const [deletable, SetDeletable] = useState(false);
-
   return (
     <>
       {replies.map((reply) => {
-        const userId = jwtDecode(localStorage.getItem("token")).userId;
-
-        if (userId === reply.writer.id) {
-          if (!deletable) {
-            SetDeletable(true);
-          }
-        } else {
-          if (deletable) {
-            SetDeletable(false);
-          }
-        }
-
-        return (
-          <div className="comment reply flex" key={reply._id}>
-            <div className="cm-main flex">
-              <Link
-                href={`/profile/${reply.writer.username}/${reply.writer.id}`}
-              >
-                <a className="flex">
-                  <p id="cm-user">{reply.writer.username}:</p>
-                </a>
-              </Link>
-              <p dir="auto" className="cm-text">
-                {reply.message}
-              </p>
-            </div>
-            <div className="cm-info">
-              <div className="flex">
-                <p id="date">{dateFormat(reply.date, "mmm d, yyyy")}</p>
-              </div>
-              {deletable && (
-                <DeleteComment onDelete={onDelete} id={reply._id} />
-              )}
-            </div>
-          </div>
-        );
+        return <Reply reply={reply} onDelete={onDelete} key={reply._id} />;
       })}
     </>
   );
