d159517d87c71ac391a6951b6b495e4bfb40fd80
ali navidi <ali_navidi24@yahoo.com>
2022-08-14 18:44:09 +0430

    code secured

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-const { createServer } = require('http')
-const { join } = require('path')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require("http");
+const { join } = require("path");
+const { parse } = require("url");
+const next = require("next");
 
-const app = next({ dev: process.env.NODE_ENV !== 'production' })
-const handle = app.getRequestHandler()
-const port = process.env.PORT ? process.env.PORT : 3000
+const app = next({ dev: process.env.NODE_ENV !== "production" });
+const handle = app.getRequestHandler();
+const port = process.env.PORT ? process.env.PORT : 3000;
 
-app.prepare()
-  .then(() => {
-    createServer((req, res) => {
-      const parsedUrl = parse(req.url, true)
-      const { pathname } = parsedUrl
-      
-      if (pathname === '/sw.js' || /^\/(workbox|worker|fallback)-\w+\.js$/.test(pathname)) {
-        const filePath = join(__dirname, '.next', pathname)
-        app.serveStatic(req, res, filePath)
-      } else {
-        handle(req, res, parsedUrl)
-      }
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://localhost:${port}`)
-    })
-  })
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+
+    if (
+      pathname === "/sw.js" ||
+      /^\/(workbox|worker|fallback)-\w+\.js$/.test(pathname)
+    ) {
+      const filePath = join(__dirname, ".next", pathname);
+      app.serveStatic(req, res, filePath);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
