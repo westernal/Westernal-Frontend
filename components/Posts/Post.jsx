4cad3a4f9c3b16c6f5c3c4f5e5511a656fd156cf
ali navidi <ali_navidi24@yahoo.com>
2022-08-16 10:26:20 +0430

    content loader changed

--- components/Posts/Post.jsx
+++ components/Posts/Post.jsx
@@ -14,8 +14,16 @@ const Post = ({ details, onDelete, deletable = false }) => {
   const [user, SetUser] = useState();
   const [isSpotify, SetIsSpotify] = useState(false);
   const [error, SetError] = useState(false);
+  const [bgColor, SetBgColor] = useState("#f3f3f3");
 
   useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      SetBgColor("#5f5d5d");
+    }
+
     if (details.songUrl) {
       if (details.songUrl.toLowerCase().includes("spotify")) {
         SetIsSpotify(true);
@@ -54,18 +62,18 @@ const Post = ({ details, onDelete, deletable = false }) => {
             speed={2}
             width={"100%"}
             height={"100%"}
-            viewBox="0 0 320 320"
-            backgroundColor="#f3f3f3"
+            viewBox="0 0 320 420"
+            backgroundColor={bgColor}
             foregroundColor="#ecebeb"
           >
             <circle cx="25" cy="29" r="11" />
             <rect x="45" y="25" rx="2" ry="2" width="100" height="10" />
-            <rect x="0" y="54" rx="2" ry="2" width="320" height="176" />
-            <rect x="12" y="245" rx="0" ry="0" width="90" height="12" />
-            <rect x="12" y="265" rx="0" ry="0" width="130" height="10" />
-            <circle cx="20" cy="305" r="9" />
-            <rect x="40" y="297" rx="0" ry="0" width="17" height="17" />
-            <rect x="215" y="300" rx="0" ry="0" width="90" height="12" />
+            <rect x="0" y="54" rx="2" ry="2" width="320" height="276" />
+            <rect x="12" y="345" rx="0" ry="0" width="90" height="12" />
+            <rect x="12" y="365" rx="0" ry="0" width="130" height="10" />
+            <circle cx="20" cy="405" r="9" />
+            <rect x="40" y="397" rx="0" ry="0" width="17" height="17" />
+            <rect x="215" y="400" rx="0" ry="0" width="90" height="12" />
           </ContentLoader>
         )}
         {user && (
