8cca1c2c13b70e5309988bead032365b24081def
ali navidi <ali_navidi24@yahoo.com>
2022-08-18 17:05:01 +0430

    bugs fixed

--- components/authentication/CheckToken.jsx
+++ components/authentication/CheckToken.jsx
@@ -3,6 +3,12 @@ import { useEffect } from "react";
 
 const CheckToken = () => {
   const router = useRouter();
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      router.push("/");
+    }
+  }, []);
 };
 
 export default CheckToken;
