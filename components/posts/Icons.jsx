0e66c336b8210b07c4c7eb1290440aa1ce7bd1d5
ali navidi <ali_navidi24@yahoo.com>
2022-08-22 14:11:53 +0430

    style changed

--- components/posts/Icons.jsx
+++ components/posts/Icons.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Icons = () => {
   return (
     <div className="icons">
-      <Image src={"/Images/spotify.svg"} width={25} height={25} alt="spotify" />
+      <Image src={"/Images/spotify.svg"} width={27} height={27} alt="spotify" />
       <Image
         src={"/Images/soundcloud.svg"}
         width={46}
@@ -18,7 +18,7 @@ const Icons = () => {
         height={25}
         alt="facebook"
       />
-      <Image src={"/Images/vimeo.svg"} width={25} height={25} alt="vimeo" />
+      <Image src={"/Images/vimeo.svg"} width={27} height={27} alt="vimeo" />
     </div>
   );
 };
