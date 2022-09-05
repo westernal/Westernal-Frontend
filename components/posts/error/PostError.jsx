7d9e107a6f1aaa6ab00e0c2d36ac717f40077bf2
ali navidi <ali_navidi24@yahoo.com>
2022-09-05 13:24:43 +0430

    post error changed

--- components/posts/error/PostError.jsx
+++ components/posts/error/PostError.jsx
@@ -2,9 +2,10 @@ const PostError = () => {
   return (
     <div className="post-error flex">
       <div className="error-icon">&times;</div>
-      <p id="error-text">
-        Sorry looks like there is a problem, either you don{"'"}t have access or
-        the link itself is not a media that we support!
+      <p id="error-text">Sorry, looks like there is a problem.</p>
+      <p id="error-reason">
+        either you don{"'"}t have access or the link itself is not a media that
+        we support!
       </p>
     </div>
   );
