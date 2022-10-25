698e7a8d3197a34a2b15435cd96e5bfb4c4c1582
ali navidi <ali_navidi24@yahoo.com>
2022-10-25 15:50:07 +0330

    bugs fixed

--- components/layout/PWAModal.jsx
+++ components/layout/PWAModal.jsx
@@ -24,36 +24,43 @@ const PWAModal = () => {
     }
   }, []);
   return (
-    <div id="myModal" className="modal">
-      <div className="modal-content flex">
-        <div className="modal-main auth-form">
-          <div className="flex">
-            <Image
-              src={"/Images/logo.png"}
-              alt="logo"
-              width={150}
-              height={150}
-            />
-          </div>
-          <div className="flex">
-            <h3>Download the Westernal app and enjoy!</h3>
-          </div>
-          <div className="share-info">
-            <div className="grid">
-              <Image width={35} height={35} src={share1.src} alt="share" />
-              <p>Click on share button.</p>
+    <div className="pwa">
+      <div id="myModal" className="modal">
+        <div className="modal-content flex">
+          <div className="modal-main auth-form">
+            <div className="flex">
+              <Image
+                src={"/Images/logo.png"}
+                alt="logo"
+                width={150}
+                height={150}
+              />
             </div>
-            <div className="grid">
-              <Image width={35} height={35} src={share2.src} alt="HomeScreen" />
-              <p>Choose Add To Homescreen.</p>
+            <div className="flex">
+              <h3>Download the Westernal app and enjoy!</h3>
             </div>
-            <div className="grid">
-              <Image width={35} height={35} src={share3.src} alt="Add" />
-              <p>Click Add.</p>
+            <div className="share-info">
+              <div className="grid">
+                <Image width={35} height={35} src={share1.src} alt="share" />
+                <p>Click on share button.</p>
+              </div>
+              <div className="grid">
+                <Image
+                  width={35}
+                  height={35}
+                  src={share2.src}
+                  alt="HomeScreen"
+                />
+                <p>Choose Add To Homescreen.</p>
+              </div>
+              <div className="grid">
+                <Image width={35} height={35} src={share3.src} alt="Add" />
+                <p>Click Add.</p>
+              </div>
             </div>
-          </div>
-          <div className="flex">
-            <button className="btn close1">Got it</button>
+            <div className="flex">
+              <button className="btn close1">Got it</button>
+            </div>
           </div>
         </div>
       </div>
