144b58594b6172a6acba83ec8c406316f621d9a2
ali navidi <ali_navidi24@yahoo.com>
2022-11-04 17:19:53 +0330

    bugs fixed

--- components/layout/PWAModal.jsx
+++ components/layout/PWAModal.jsx
@@ -15,12 +15,6 @@ const PWAModal = () => {
       span.addEventListener("click", function () {
         modal.style.display = "none";
       });
-
-      window.onclick = function (event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      };
     }
   }, []);
   return (
