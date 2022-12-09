373105feea8d62db53f1af5c9ce95c53bf591c33
ali navidi <ali_navidi24@yahoo.com>
2022-12-09 16:41:49 +0330

    spotify embed added

--- components/player/SpotifyPlayer.jsx
+++ components/player/SpotifyPlayer.jsx
@@ -3,36 +3,16 @@ import { useEffect } from "react";
 import getSpotifyToken from "../../requests/getSpotifyToken";
 
 const SpotifyPlayer = ({ url }) => {
-  const [spotifyEmbed, SetSpotifyEmbed] = useState();
+  const [link, SetLink] = useState(new URL(url));
 
-  useEffect(() => {
-    getSpotifyEmbed();
-  }, []);
-
-  const getSpotifyEmbed = async () => {
-    const token = await getSpotifyToken();
-
-    const options = {
-      method: "GET",
-      headers: {
-        "content-type": "application/json+oembed",
-      },
-    };
-
-    const response = await fetch(
-      `https://open.spotify.com/oembed?url=${url}`,
-      options
-    );
-
-    const data = await response.json();
-    const status = response.status;
-
-    console.log(data);
-
-    SetSpotifyEmbed(data.html);
-  };
-
-  return <div className="spotify-player">{spotifyEmbed && spotifyEmbed}</div>;
+  return (
+    <iframe
+      src={`https://open.spotify.com/embed${link.pathname}`}
+      frameBorder="0"
+      width={"100%"}
+      height={"360px"}
+    ></iframe>
+  );
 };
 
 export default SpotifyPlayer;
