e074d604448d35f2e388abf4dd5ac166492a42e3
ali navidi <ali_navidi24@yahoo.com>
2022-12-09 22:22:05 +0330

    bugs fixed

--- components/player/SpotifyPlayer.jsx
+++ components/player/SpotifyPlayer.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import { useEffect } from "react";
 import getSpotifyToken from "../../requests/getSpotifyToken";
 
-const SpotifyPlayer = ({ url }) => {
+const SpotifyPlayer = ({ url, height = "360px" }) => {
   const [link, SetLink] = useState(new URL(url));
 
   return (
     <iframe
       src={`https://open.spotify.com/embed${link.pathname}`}
-      frameBorder="0"
+      frameborder="0"
       width={"100%"}
-      height={"360px"}
+      height={height}
     ></iframe>
   );
 };
