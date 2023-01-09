e3483ca9617df42ee46db952d7e51c2ed548adbc
ali navidi <ali_navidi24@yahoo.com>
2023-01-10 00:36:01 +0330

    bugs fixed

--- Functions/formatDate.jsx
+++ Functions/formatDate.jsx
@@ -6,26 +6,44 @@ export default function formatDate(date) {
   var interval = seconds / 31536000;
 
   if (interval > 1) {
-    return Math.floor(interval) + " years ago";
+    const time = Math.floor(interval);
+    if (time === 1) {
+      return "1 year ago";
+    } else return time + " years ago";
   }
   interval = seconds / 2592000;
   if (interval > 1) {
-    return Math.floor(interval) + " months ago";
+    const time = Math.floor(interval);
+    if (time === 1) {
+      return "1 month ago";
+    } else return time + " months ago";
   }
   interval = seconds / 86400;
   if (interval > 1) {
-    return Math.floor(interval) + " days ago";
+    const time = Math.floor(interval);
+    if (time === 1) {
+      return "1 day ago";
+    } else return time + " days ago";
   }
   interval = seconds / 3600;
   if (interval > 1) {
-    return Math.floor(interval) + " hours ago";
+    const time = Math.floor(interval);
+    if (time === 1) {
+      return "1 hour ago";
+    } else return time + " hours ago";
   }
   interval = seconds / 60;
   if (interval > 1) {
-    return Math.floor(interval) + " minutes ago";
+    const time = Math.floor(interval);
+    if (time === 1) {
+      return "1 minute ago";
+    } else return time + " minutes ago";
   }
   if (interval <= 0) {
     return "Just now";
   }
-  return Math.floor(seconds) + " seconds ago";
+  const time = Math.floor(seconds);
+  if (time === 1) {
+    return "1 second ago";
+  } else return time + " seconds ago";
 }
