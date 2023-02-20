30f2e008e15d4539624dc378ba9d1cc7486e3bac
ali navidi <ali_navidi24@yahoo.com>
2023-02-20 16:01:54 +0330

    bugs fixed

--- components/user/Users.jsx
+++ components/user/Users.jsx
@@ -10,28 +10,31 @@ const User = ({ users }) => {
       {!users &&
         [1, 2, 3, 4, 5, 6, 7].map((elem, index) => {
           return (
-            <div className="profile-notif flex" key={index}>
+            <div className="user profile-notif flex" key={index}>
               <UserLoader />
             </div>
           );
         })}
       {users &&
         users.map((user) => (
-          <Link href={`/${user.username}`} className="flex" key={user._id}>
-            <div className="profile-notif flex">
-              {
-                <Image
-                  src={
-                    !user.image.includes("userIcon")
-                      ? host + user.image
-                      : "/Images/user.svg"
-                  }
-                  width={50}
-                  height={50}
-                  style={{ borderRadius: "50%" }}
-                  alt="user avatar"
-                />
-              }
+          <Link
+            href={`/${user.username}`}
+            className="user flex profile-notif"
+            key={user._id}
+          >
+            <div className="flex user-info">
+              <Image
+                src={
+                  !user.image.includes("userIcon")
+                    ? host + user.image
+                    : "/Images/user.svg"
+                }
+                width={50}
+                height={50}
+                style={{ borderRadius: "50%" }}
+                alt="user avatar"
+              />
+
               <strong id="userId">{user.username} </strong>
               {user.verified && (
                 <div className="verify">
