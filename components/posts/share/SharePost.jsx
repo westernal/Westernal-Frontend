ef7b619524e62c7f38669ea215566f2fa8fa10d3
ali navidi <ali_navidi24@yahoo.com>
2023-02-26 00:15:17 +0330

    share post added

--- components/posts/share/SharePost.jsx
+++ components/posts/share/SharePost.jsx
@@ -1,14 +1,30 @@
+import { useRef } from "react";
 import { toast } from "react-toastify";
 
 const SharePost = ({ id, hide }) => {
+  const Link = useRef({
+    text: "Post",
+    title: "See this post on Westernal.",
+    url: `https://www.westernal.net/post/${id}`,
+  });
+
   const copyLink = () => {
-    let Link = `https://www.westernal.net/post/${id}`;
-    navigator.clipboard.writeText(Link);
+    navigator.clipboard.writeText(Link.current.url);
     hide();
     toast.success("Link copied!");
   };
 
-  return <li onClick={copyLink}>Copy link</li>;
+  const share = async () => {
+    await navigator.share(Link.current);
+    hide();
+  };
+
+  return (
+    <>
+      <li onClick={share}>Share post</li>
+      <li onClick={copyLink}>Copy link</li>
+    </>
+  );
 };
 
 export default SharePost;
