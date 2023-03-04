b81f9f3166e87601221631e3602ab59bb502e228
ali navidi <ali_navidi24@yahoo.com>
2023-03-04 12:06:56 +0330

    bugs fixed

--- requests/API.jsx
+++ requests/API.jsx
@@ -1,3 +1,5 @@
+import { toast } from "react-toastify";
+
 export default async function API(options, address) {
   const host = "https://alinavidi.ir/";
 
@@ -6,6 +8,12 @@ export default async function API(options, address) {
   const data = await response.json();
 
   const status = response.status;
+
+  if (status == 403) {
+    toast.error("Authentication failed, please login again.");
+    localStorage.removeItem("token");
+    location.reload();
+  }
 
   var requestResult = { status, data };
 
