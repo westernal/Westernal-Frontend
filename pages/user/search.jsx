da9e58e103d368d74b261d8926c8f74fc08b2944
ali navidi <ali_navidi24@yahoo.com>
2023-03-08 16:50:50 +0330

    useSearchUsers hook added

--- pages/user/search.jsx
+++ pages/user/search.jsx
@@ -1,55 +1,16 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import Footer from "../../components/layout/Footer";
-import API from "../../functions/requests/API";
 import User from "../../components/user/Users";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import useAuth from "../../hooks/useAuth";
+import useSearchUsers from "../../hooks/useSearchUsers";
 
 const Search = () => {
-  const [users, SetUsers] = useState();
-  const [isTyped, SetIsTyped] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const router = useRouter();
   const render = useAuth(router);
-  const controllerRef = useRef();
-
-  async function searchUsers(e) {
-    SetIsTyped(true);
-    if (controllerRef.current) {
-      controllerRef.current.abort();
-    }
-
-    const controller = new AbortController();
-    controllerRef.current = controller;
-
-    const option = {
-      signal: controllerRef.current?.signal,
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    };
-
-    try {
-      var result = await API(option, `api/users/search/${searchTerm}`);
-    } catch (error) {
-      return;
-    }
-
-    if (result.status == 200) {
-      SetUsers(result.data.users);
-      controllerRef.current = null;
-    }
-
-    if (searchTerm === "") {
-      SetIsTyped(false);
-    }
-  }
-
-  useEffect(() => {
-    if (searchTerm) {
-      searchUsers();
-    }
-  }, [searchTerm]);
+  const [isTyped, users] = useSearchUsers(searchTerm);
 
   return (
     <>
