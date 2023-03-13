e56f116114ac7b23b32d097e50624f266ff560f9
ali navidi <ali_navidi24@yahoo.com>
2023-03-13 19:10:13 +0330

    retries added

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.westernal.net/",
+    retries: {
+      runMode: 3,
+    },
   },
 });
