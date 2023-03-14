8c3d888b1489b23ccf1f76fe2a599d1a0acc38ab
ali navidi <ali_navidi24@yahoo.com>
2023-03-15 00:26:02 +0330

    changing profile picture's test added

--- cypress/e2e/usersetting.cy.js
+++ cypress/e2e/usersetting.cy.js
@@ -13,9 +13,8 @@ describe("Edit user", () => {
     cy.visit("/cypress/setting");
 
     //change the profile
-    // cy.get("#image").selectFile(
-    //   "https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ast2p3i2zthwnz5skluw.png"
-    // );
+    cy.fixture("logo.png", { encoding: null }).as("profilePicture");
+    cy.get("#image").selectFile("@profilePicture");
     cy.get("#username").clear().type("cypress");
     cy.get("#bio").clear().type("cypress test");
     cy.get("#link").clear().type("https://www.westernal.net/cypress");
