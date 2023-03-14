04d5be0ac150724dfacb080180b75459bfebb90e
ali navidi <ali_navidi24@yahoo.com>
2023-03-14 21:18:32 +0330

    bugs fixed

--- cypress/e2e/usersetting.cy.js
+++ cypress/e2e/usersetting.cy.js
@@ -13,9 +13,9 @@ describe("Edit user", () => {
     cy.visit("/cypress/setting");
 
     //change the profile
-    cy.get("#image").selectFile(
-      "C:/Users/ASUS/Desktop/Folders/Pictures/WhatsApp Image 2022-05-24 at 9.30.20 PM.jpeg"
-    );
+    // cy.get("#image").selectFile(
+    //   "https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ast2p3i2zthwnz5skluw.png"
+    // );
     cy.get("#username").clear().type("cypress");
     cy.get("#bio").clear().type("cypress test");
     cy.get("#link").clear().type("https://www.westernal.net/cypress");
