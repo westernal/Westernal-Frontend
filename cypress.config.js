b805cd859a3e3faf74eec0ca9f356ad92f2b86e8
ali navidi <ali_navidi24@yahoo.com>
2023-03-14 21:07:11 +0330

    bugs fixed

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
     specPattern: [
       "cypress/e2e/login.cy.js",
       "cypress/e2e/searchUser.cy.js",
-      "cypress/e2e/userSetting.cy.js",
+      "cypress/e2e/usersetting.cy.js",
       "cypress/e2e/followUser.cy.js",
       "cypress/e2e/unfollowUser.cy.js",
       "cypress/e2e/createPost.cy.js",
