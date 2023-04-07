adc6686202ebeb19fb37804eddaebf856fa59c62
ali navidi <ali_navidi24@yahoo.com>
2023-04-08 01:28:22 +0330

    Bugs fixed

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  publicExcludes: ["!robots.txt", "!sitemap.xml", "!sitemap-0.xml"],
 });
 
 module.exports = withPWA({
