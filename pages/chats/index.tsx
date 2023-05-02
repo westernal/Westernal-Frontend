fc0f7240627cac2e4ecbc6991408f91fb3d1cc73
ali navidi <ali_navidi24@yahoo.com>
2023-05-02 17:11:36 +0330

    Get chats added

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,5 +1,23 @@
-const Chats = () => {
+import { getCookie } from "cookies-next";
+import getRequest from "../../functions/requests/getRequest";
+
+const Chats = ({ chats }: { chats: any }) => {
   return <main>hello</main>;
+};
+
+Chats.getInitialProps = async (req, res) => {
+  const userId = getCookie("cookieToken", { req, res });
+  const result = await getRequest(`api/chats/${userId}`);
+
+  if (result?.status == 404 || !result) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    chats: result.data.chats,
+  };
 };
 
 export default Chats;
