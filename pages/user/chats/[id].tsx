f14490a4c987317bc10a908c925b34124c094366
ali navidi <ali_navidi24@yahoo.com>
2023-05-03 14:15:43 +0330

    Chat page added

---
+++ pages/user/chats/[id].tsx
@@ -0,0 +1,12 @@
+import BackHeader from "../../../components/layout/header/BackHeader";
+
+const Chat = () => {
+  return (
+    <>
+      <BackHeader title="username" />
+      <main className="chat"></main>
+    </>
+  );
+};
+
+export default Chat;
