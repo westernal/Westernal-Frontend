fa904a234ac6333347506f3f42f5fd54efb4a7c3
ali navidi <ali_navidi24@yahoo.com>
2023-05-06 05:18:44 +0330

    Reset message count added

--- pages/user/chats/[id].tsx
+++ pages/user/chats/[id].tsx
@@ -2,11 +2,33 @@ import { mutate } from "swr";
 import ChatInput from "../../../components/chats/UI/chatInput";
 import Messages from "../../../components/chats/UI/messages";
 import BackHeader from "../../../components/layout/header/BackHeader";
+import { useEffect } from "react";
+import decodeJWT from "../../../functions/decodeJWT";
+import { getCookie } from "cookies-next";
+import { useRouter } from "next/router";
+import postRequest from "../../../functions/requests/postRequest";
 
 const Chat = () => {
+  const router = useRouter();
+
   const onMessageSent = (id: string) => {
     mutate(`api/chats/chat/messages/${id}`);
   };
+
+  useEffect(() => {
+    const resetMessageCount = async () => {
+      const senderId = decodeJWT(getCookie("cookieToken").toString()).userId;
+      const result = await postRequest(
+        {
+          chatId: router.query.id,
+        },
+        `api/users/messages/reset/${senderId}`,
+        true
+      );
+    };
+
+    resetMessageCount();
+  }, []);
   return (
     <>
       <BackHeader title="Chat" />
