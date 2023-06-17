36ee93c86669250c6394463ba34540d5a04e8a7b
ali navidi <ali_navidi24@yahoo.com>
2023-06-18 01:04:18 +0330

    Post options tests added

---
+++ tests/components/posts/options/PostOptions.spec.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PostOptions from "../../../../components/posts/options/PostOptions";
+import { render } from "@testing-library/react";
+
+describe("Post options component", () => {
+  it.only("Post options shows delete option when the post is deletable", () => {
+    // const mockFn = jest.fn();
+    // const POST_OPTIONS = render(
+    //   <PostOptions
+    //     onDelete={mockFn}
+    //     isLoggedIn={true}
+    //     deletable={true}
+    //     onUnsave={mockFn}
+    //   />
+    // );
+    // POST_OPTIONS.findByText("Delete post");
+  });
+});
