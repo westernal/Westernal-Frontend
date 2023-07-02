903baa81657a41c1816c89dd4a289491b0f6c962
ali navidi <ali_navidi24@yahoo.com>
2023-07-02 23:21:28 +0330

    Bugs fixed

--- components/posts/options/PostOptions.tsx
+++ components/posts/options/PostOptions.tsx
@@ -8,36 +8,31 @@ import { usePostContext } from "../../../context/postContext";
 const PostOptions = ({ onDelete, isLoggedIn, deletable, onUnsave }) => {
   const post = usePostContext();
 
-  const openMenu = () => {
+  const toggleMenu = () => {
     const menu = document.getElementById(post._id);
-    menu.style.maxHeight = "600px";
-  };
-
-  const closeMenu = () => {
-    const menu = document.getElementById(post._id);
-    menu.style.maxHeight = "0px";
+    menu.classList.toggle("active");
   };
 
   return (
     <section className="post-options flex">
-      <button id="more" className="flex" onClick={openMenu}>
+      <button id="more" className="flex" onClick={toggleMenu}>
         ...
       </button>
       <ul className="post-menu" id={post._id}>
-        <SharePost id={post._id} hide={closeMenu} />
+        <SharePost id={post._id} hide={toggleMenu} />
         {isLoggedIn ? (
-          <SavePost id={post._id} hide={closeMenu} onUnsave={onUnsave} />
+          <SavePost id={post._id} hide={toggleMenu} onUnsave={onUnsave} />
         ) : null}
         {deletable ? (
           <li>
             <Link href={`/post/edit/${post._id}`}>Edit post</Link>
           </li>
         ) : null}
-        <RedirectPost id={post._id} hide={closeMenu} />
+        <RedirectPost id={post._id} hide={toggleMenu} />
         {deletable ? (
-          <DeletePost onDelete={onDelete} id={post._id} hide={closeMenu} />
+          <DeletePost onDelete={onDelete} id={post._id} hide={toggleMenu} />
         ) : null}
-        <li onClick={closeMenu}>Cancel</li>
+        <li onClick={toggleMenu}>Cancel</li>
       </ul>
     </section>
   );
